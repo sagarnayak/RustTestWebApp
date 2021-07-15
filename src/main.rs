c23f680e85c73c7ab90092e0dffb199865f0540e
sagar <sagar@oneolabs.com>
2021-07-15 18:13:22 +0530

    added schedulers

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate rocket;
 extern crate tokio;
 
 
+use schedules::schedule_master::schedule;
+
 use crate::core::rocket_master::rocket;
 
 mod core;
@@ -12,11 +14,12 @@ mod controllers;
 mod contracts;
 mod enums;
 mod model;
-
+mod schedules;
 mod config_controller;
 pub mod database;
 
 #[launch]
 fn init_main() -> _ {
+    schedule();
     rocket()
 }
---
+++ src/schedules/schedule_master.rs
@@ -0,0 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
+use chrono::{Local, Utc};
+use clokwerk::Scheduler;
+use clokwerk::Interval::*;
+
+pub fn schedule() {
+    let mut scheduler = Scheduler::with_tz(Utc);
+
+    scheduler.every(
+        Seconds(10)
+    )
+        .run(
+            || {
+                println!("Current time is {}", Local::now());
+            }
+        );
+
+    thread::spawn(
+        move || {
+            loop {
+                scheduler.run_pending();
+                thread::sleep(Duration::from_millis(1000))
+            }
+        }
+    );
+}
